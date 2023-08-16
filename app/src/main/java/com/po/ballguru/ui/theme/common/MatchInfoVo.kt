da68d8c3561c0407ccc24f24ec713ffdd4f925ae
Nyi Ye Han <nyiyehan@gmail.com>
2023-08-16 23:42:21 +0630

    update for ui

--- app/src/main/java/com/po/ballguru/ui/theme/common/MatchInfoVo.kt
+++ app/src/main/java/com/po/ballguru/ui/theme/common/MatchInfoVo.kt
@@ -3,6 +3,8 @@ package com.po.ballguru.ui.theme.common
 data class MatchInfoVo(
     val match_name: String? = null,
     val first_team: String? = null,
+    val first_team_logo: String? = null,
+    val second_team_logo: String? = null,
     val second_team: String? = null,
     val link: String? = null,
     val quality: String? = null,

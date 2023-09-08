9357d85f2118548c060e853be1c66b591148f920
Shine Thyu Zan <shinethyuzan2010@gmail.com>
2023-09-08 11:44:56 +0630

    app v1

--- app/src/androidTest/java/com/po/ballguru/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/po/ballguru/ExampleInstrumentedTest.kt
@@ -1,10 +1,9 @@
 package com.po.ballguru
 
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
+
 
 import org.junit.Test
-import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
@@ -13,7 +12,6 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
